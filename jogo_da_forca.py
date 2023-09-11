a09cd518619e9ec4b51cb5b05a104f343307aa35
Jessé Silva <jesse.silva@ezlogic.com.br>
2023-09-10 21:52:44 -0300

    Adicionei o código do jogo da forca em Python - Versão Final
    Este commit adiciona a versão final do jogo da forca em Python, incluindo importações de bibliotecas, funções para limpar a tela, exibir a forca, validar entrada do jogador, carregar palavras, escolher categoria e palavra aleatória, processar jogadas do jogador e a função principal do jogo. A versão final do jogo inclui todos os recursos e está pronto para ser jogado.
    
    Detalhes adicionais:
    - Importação de bibliotecas para funcionalidades essenciais.
    - Funções para desenhar a forca, limpar a tela e processar jogadas.
    - Carregamento de palavras a partir de um arquivo JSON.
    - Escolha aleatória de categoria e palavra.
    - Exibição de dicas e atualização do tabuleiro.
    - Lógica de vitória ou derrota do jogador.
    Este jogo pode ser executado localmente em um ambiente Python e fornece uma experiência de jogo interativa. Todas as funcionalidades foram implementadas e testadas com sucesso.

--- jogo_da_forca.py
+++ jogo_da_forca.py
@@ -1,211 +1,221 @@
-# Projeto 1 - Desenvolvimento de Game em Linguagem Python - Versão 1
+# Projeto 1 - Desenvolvimento de Game em Linguagem Python - Versão Final
 
-#import
-import random 
-from os import system, name 
+# Importação de bibliotecas
+import json
+import sys
+import random
+import re
+from os import system, name
 
-#Função para limar a tela a cada execução
+# Constante para a mensagem de dica
+DICA_MESSAGE = 'A dica é: {}'
+
+# Função para limpar a tela a cada execução
 def limpa_tela():
-
-    #Windows
-    if name == 'nt':
-        _ = system('cls')
-
-    #mac ou linux
-    else:
-        _ = system('clear')
+    try:
+        # Lógica para limpar a tela (depende do ambiente em que você está executando o código)
+        # Windows
+        if name == 'nt':
+            _ = system('cls')
+        # Mac ou Linux
+        else:
+            _ = system('clear')
+    except Exception as e:
+        print(f"Erro ao limpar a tela: {e}")
 
 # Função que desenha a forca na tela
-def display_hangman(chances):      
-    # Lista de estágios da forca
-    stages = [  # estágio 6 (final)
-                """
-                   --------
-                   |      |
-                   |      O
-                   |     \\|/
-                   |      |
-                   |     / \\
-                   -
-                """,
-                # estágio 5
-                """
-                   --------
-                   |      |
-                   |      O
-                   |     \\|/
-                   |      |
-                   |     / 
-                   -
-                """,
-                # estágio 4
-                """
-                   --------
-                   |      |
-                   |      O
-                   |     \\|/
-                   |      |
-                   |      
-                   -
-                """,
-                # estágio 3
-                """
-                   --------
-                   |      |
-                   |      O
-                   |     \\|
-                   |      |
-                   |     
-                   -
-                """,
-                # estágio 2
-                """
-                   --------
-                   |      |
-                   |      O
-                   |      |
-                   |      |
-                   |     
-                   -
-                """,
-                # estágio 1
-                """
-                   --------
-                   |      |
-                   |      O
-                   |    
-                   |      
-                   |     
-                   -
-                """,
-                # estágio 0
-                """
-                   --------
-                   |      |
-                   |      
-                   |    
-                   |      
-                   |     
-                   -
-                """
-    ]
-    return stages[chances]    
+def display_hangman(chances):
+    try:
+        # Lógica para exibir a imagem do enforcado com base nas chances restantes
+        stages = [
+            # Estágio 6 (final)
+            """
+                --------
+                |      |
+                |      O
+                |     \\|/
+                |      |
+                |     / \\
+                -
+            """,
+            # Estágio 5
+            """
+                --------
+                |      |
+                |      O
+                |     \\|/
+                |      |
+                |     / 
+                -
+            """,
+            # Estágio 4
+            """
+                --------
+                |      |
+                |      O
+                |     \\|/
+                |      |
+                |      
+                -
+            """,
+            # Estágio 3
+            """
+                --------
+                |      |
+                |      O
+                |     \\|
+                |      |
+                |     
+                -
+            """,
+            # Estágio 2
+            """
+                --------
+                |      |
+                |      O
+                |      |
+                |      |
+                |     
+                -
+            """,
+            # Estágio 1
+            """
+                --------
+                |      |
+                |      O
+                |    
+                |      
+                |     
+                -
+            """,
+            # Estágio 0
+            """
+                --------
+                |      |
+                |      
+                |    
+                |      
+                |     
+                -
+            """
+        ]
+        return stages[chances]
+    except Exception as e:
+        print(f"Erro ao exibir imagem do enforcado: {e}")
 
-def game():
+# Função para validar a entrada do jogador
+def validar_entrada(tentativa):
+    padrao = r'^[a-zA-Z]$'
+    return re.match(padrao, tentativa) is not None
 
-    limpa_tela()
-    print('\nBem-vindo(a) ao jogo da forca')
-    print('Adivinhe a palavra abaixo: \n')
+# Função para carregar palavras do arquivo JSON
+def carregar_palavras():
+    try:
+        # Lógica para carregar palavras
+        with open('jogo_da_forca/categorias.json', 'r', encoding='utf-8') as arquivo:
+            dados = json.load(arquivo)
+            return dados
+    except Exception as e:
+        print(f"Erro ao carregar palavras: {e}")
+        return []
 
-    #lista de palavras para o jogo
-    palavras = [
-    "Maçã",
-    "Banana",
-    "Laranja",
-    "Pera",
-    "Uva",
-    "Morango",
-    "Abacaxi",
-    "Melancia",
-    "Limão",
-    "Manga",
-    "Cereja",
-    "Kiwi",
-    "Pêssego",
-    "Abacate",
-    "Framboesa",
-    "Amora",
-    "Goiaba",
-    "Caju",
-    "Coco",
-    "Tangerina",
-    "Maracujá",
-    "Pitanga",
-    "Jabuticaba",
-    "Ameixa",
-    "Açaí",
-    "Acerola",   
-    "Groselha",
-    "Caqui",    
-    "Pomelo",
-    "Figo",
-    "Papaya",    
-    "Mamão",
-    "Carambola",    
-    "Guaraná",    
-    "Romã",
-    "Melão",    
-    "Jambo",
-    "Tâmara",     
-    "Tamarindo",
-    "Noz-moscada",
-    "Sapoti"
-]
+# Função para escolher uma categoria aleatória
+def escolher_categoria(palavras):
+    try:
+        # Lógica para escolher a categoria
+        return random.choice(list(palavras.keys()))
+    except Exception as e:
+        print(f"Erro ao escolher categoria: {e}")
+        return "categoria_padrão"
 
-    #escolhe randomicamente uma palavra            
-    palavra = random.choice(palavras).lower()
+# Função para escolher uma palavra aleatória da categoria
+def escolher_palavra(palavras, categoria):
+    try:
+        # Lógica para escolher a palavra
+        return random.choice(palavras[categoria]).lower()
+    except Exception as e:
+        print(f"Erro ao escolher palavra: {e}")
+        return "palavra_padrão"
 
-    print("A dica é: FRUTA\n")
-    
-    #list comprehesion
-    lista_letras_palavras = [letra for letra in palavra]
-
-    #Cria o tabuleiro com o caracter "_" multiplicado pelo comprimento da palavra
-    tabuleiro = ["_"] * len(palavra)
-
-    # Número de chances
-    chances = 6
-
-    #lista para as letras digitadas
-    letras_tentativas = []
-
-    #Loop enquanto número de chances for maior do que zero
-    while chances > 0 :
-
-        #print
+# Função para processar a jogada do jogador
+def handle_turn(chances, tabuleiro, letras_tentadas, lista_letras_palavras, categoria):
+    try:
         print(display_hangman(chances))
-        print("\nPalavra: ", tabuleiro)
-        print("\n")
+        print('\nPalavra: ', ' '.join(tabuleiro))
+        print(DICA_MESSAGE.format(categoria))
+        print('\n')
+        sys.stdout.flush()
 
-        #Tentativa
-        tentativa = input("\nDigite uma letra: ")
+        tentativa = input('\nDigite uma letra: ').lower()
 
-        #condicional
-        if tentativa in letras_tentativas:
+        if not validar_entrada(tentativa):
+            print('Entrada inválida. Por favor, digite apenas uma letra.')
+            return chances, False
+
+        if tentativa in letras_tentadas:
+            print(DICA_MESSAGE.format(categoria))
+            print(letras_tentadas)
             print('Você já tentou essa letra. Escolha outra!')
-            continue
+            return chances, False
 
-        #lista de tentativas (letras)
-        letras_tentativas.append(tentativa)    
+        letras_tentadas.append(tentativa)
 
-        #condicional
         if tentativa in lista_letras_palavras:
-            print("\nVocê acertou a letra!")    
+            print(DICA_MESSAGE.format(categoria))
+            print('\nVocê acertou a letra')
 
-            #loop
-            for indice in range(len(lista_letras_palavras)):
-                #condicional
-                if lista_letras_palavras[indice] == tentativa:
-                    tabuleiro[indice] = tentativa
-
-            #se todos os espaços foram preenchidos, o jogo acabou
-            if "_" not in tabuleiro: 
-                print('\nVocê venceu! aA palavra era: {}'.format(palavra))
-                break
+            for indice, letra in enumerate(lista_letras_palavras):
+                if letra == tentativa:
+                    tabuleiro[indice] = letra
+            return chances, '_' not in tabuleiro
         else:
-            print('Ops. Essa letra não está na palavra!')
-            #decremento
-            chances -= 1           
-                    
-           
+            print(DICA_MESSAGE.format(categoria), '\n')
+            print(f'\nOps. A letra {tentativa} não está na palavra!')
+            print('\nVocê não acertou nenhuma letra nesse turno.')
+            chances -= 1
+    except Exception as e:
+        print(f"Erro durante o jogo: {e}")
 
-    #condicional 
-    if "_" in tabuleiro:
-        print("\nVocê perdeu! A palavra era: {}".format(palavra))
+    return chances, False
 
+# Função principal do jogo
+def game():
+    try:
+        palavras = carregar_palavras()
 
-#bloco main
+        if not palavras:
+            print("Não foi possível carregar as palavras. O jogo não pode continuar.")
+            return
 
+        categoria = escolher_categoria(palavras)
+        palavra_secreta = escolher_palavra(palavras, categoria)
+
+        limpa_tela()
+
+        print('\nBem-vindo(a) ao jogo da forca')
+        print('Adivinhe a palavra abaixo: \n')
+
+        print(DICA_MESSAGE.format(categoria))
+
+        lista_letras_palavras = [letra.lower() for letra in palavra_secreta]
+        tabuleiro = ['_' if letra != ' ' else ' ' for letra in palavra_secreta]
+        chances = 6
+        letras_tentadas = []
+
+        while chances > 0:
+            chances, ganhou = handle_turn(
+                chances, tabuleiro, letras_tentadas, lista_letras_palavras, categoria)
+            if ganhou:
+                print('\nVocê venceu! A palavra era: {}'.format(palavra_secreta))
+                break
+
+        if chances == 0:
+            print(display_hangman(chances))
+            print('\nVocê perdeu! A palavra era: {}'.format(palavra_secreta))
+
+    except Exception as e:
+        print(f"Erro no jogo: {e}")
+
+# Bloco principal
 if __name__ == "__main__":
     game()
-    print("\n Parabéns. Você está aprendendo programação em Python com a Data Science Academy :)\n")
